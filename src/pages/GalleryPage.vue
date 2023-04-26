8e6b9b4cd80d7cab66e28d88a9937cf5f0d5c489
Imants Liepiņš <iliepins01@gmail.com>
2023-04-26 23:13:47 +0300

    add github workflow

--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -7,12 +7,12 @@
     </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue';
 
 const dogImages = ref([]);
 
-async function fetchDogImages(count) {
+async function fetchDogImages(count: number) {
     try {
         const response = await fetch(`https://dog.ceo/api/breeds/image/random/${count}`);
         const data = await response.json();
