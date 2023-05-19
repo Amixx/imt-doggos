f0e5aa260796ca8edcbc15a1e323e7e1012d90fa
Imants Liepiņš <iliepins01@gmail.com>
2023-05-19 21:34:47 +0300

    add ratings to similar breeds page

---
+++ src/components/Rating.vue
@@ -0,0 +1,16 @@
+<template>
+  <div typeof="AggregateRating">
+    <h2 property="name">Overall Rating</h2>
+    <p>
+      {{ rating.breedName }} gets an average rating of
+      <span property="ratingValue">{{ rating.ratingValue }}</span>
+      out of
+      <span property="bestRating">{{ rating.bestRating }}</span>.
+    </p>
+    <meta property="ratingCount" content="100" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ rating: { breedName: string; ratingValue: number; bestRating: number } }>();
+</script>
